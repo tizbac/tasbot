996f55fb02ff5eaa41ed4ee510d2bc22a9de207d
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-03-06 15:58:07 +0100

    fixed error with unloadplugin and killed threads

--- Plugin.py
+++ Plugin.py
@@ -8,8 +8,8 @@ def _async_raise(tid, exctype):
     if not inspect.isclass(exctype):
         raise TypeError("Only types can be raised (not instances)")
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
-    if res == 0:
-        raise ValueError("invalid thread id")
+    #if res == 0:
+    #    raise ValueError("invalid thread id")
     elif res != 1:
         # """if it returns a number greater than one, you're in trouble, 
         # and you should call it again with exc=NULL to revert the effect"""
