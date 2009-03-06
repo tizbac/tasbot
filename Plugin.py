e6eb06174de62eafb5be072bc3d92bdf79fbaf6c
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-03-06 15:59:05 +0100

    ops

--- Plugin.py
+++ Plugin.py
@@ -10,7 +10,7 @@ def _async_raise(tid, exctype):
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
     #if res == 0:
     #    raise ValueError("invalid thread id")
-    elif res != 1:
+    if res != 1:
         # """if it returns a number greater than one, you're in trouble, 
         # and you should call it again with exc=NULL to revert the effect"""
         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
